93cb8c29870630edf5717ed0aaee43a32ef63df8
EricksonDourado <85401514+EricksonDourado@users.noreply.github.com>
2023-08-07 21:48:00 -0300

    Crud com Admin do Django

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+class config_gride_fotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id","nome",)
+    search_fields = ("nome",)
+
+
+admin.site.register(Fotografia,config_gride_fotografia)
 
 # Register your models here.
