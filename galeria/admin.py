f6bf1a252cebcbde7418f071060e178ebf78aa89
EricksonDourado <85401514+EricksonDourado@users.noreply.github.com>
2023-08-13 18:53:46 -0300

    incluso funcionalidade buscar

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
+
 from galeria.models import Fotografia
 
-class config_gride_fotografia(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
-    list_display_links = ("id","nome",)
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id","nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
-    list_per_page = 1
+    list_editable = ("publicada",)
+    list_per_page = 10
 
-
-admin.site.register(Fotografia,config_gride_fotografia)
-
-# Register your models here.
+admin.site.register(Fotografia, ListandoFotografias)
